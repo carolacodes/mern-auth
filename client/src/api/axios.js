0468cc381f245a15e5540afaf5daad634086db51
carolacodes <carolacardozog23@gmail.com>
2025-12-28 23:55:43 -0300

    deploy frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     withCredentials: true, // para enviar cookies en solicitudes CORS
 })
 
